13e58c822f55cb4044001af86bbb89274a1cee5f
Farid Store <faridjafar04@gmail.com>
2025-10-18 22:27:01 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -18,7 +18,7 @@ export default async function handler(request, response) {
   }
 
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); // Menggunakan Gemini 1.5 Flash
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" }); // Menggunakan Gemini 1.5 Flash
 
   try {
     let systemInstruction = "";
